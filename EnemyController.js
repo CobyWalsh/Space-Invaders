dc93c328793e141ab8affc321441b91dfb766dfe
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-20 14:54:52 -0700

    creating enemy rows

--- EnemyController.js
+++ EnemyController.js
@@ -1,9 +1,24 @@
 export default class EnemyController {
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+    enemyRows = [];
+    
     constructor(canvas) {
       this.canvas = canvas;
+      this.createEnemies();
    } 
 
-   draw(ctx) {
-    console.log("draw");
+   draw(ctx) {}
+
+   createEnemies() {
+    this.enemyMap.forEach((row, rowIndex) => {
+        this.enemyRows[rowIndex] = [];
+    })
    }
 }
