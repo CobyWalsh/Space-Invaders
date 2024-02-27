cb039f971d4b8026fc1f77a2823a092e2cd9aa5b
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-26 20:56:27 -0700

    my enemies now move to the right

--- EnemyController.js
+++ EnemyController.js
@@ -39,6 +39,7 @@ export default class EnemyController {
 
    drawEnemies(ctx) {
     this.enemyRows.flat().forEach((enemy) => {
+        enemy.move(this.xVelocity, this.yVelocity);
         enemy.draw(ctx);
     });
    }
