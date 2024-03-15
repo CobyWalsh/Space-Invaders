2dd78fddceb3dd417848d0ab4ee9148fbd102c3b
Coby Walsh <cobywalsh7@yahoo.com>
2024-03-15 11:43:22 -0600

    added downleft and downright enemy direction

--- EnemyController.js
+++ EnemyController.js
@@ -16,6 +16,8 @@ export default class EnemyController {
     yVelocity = 0;
     defaultXVelocity = 1;
     defaultYVelocity = 1;
+    moveDownTimerDefault = 30;
+    moveDownTimer = this.moveDownTimerDefault;
 
     
     constructor(canvas) {
@@ -24,8 +26,26 @@ export default class EnemyController {
    } 
 
    draw(ctx) {
+    this.decrementMoveDownTimer();
     this.updateVelocityAndDirection();
     this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+    console.log(this.moveDownTimer);
+   }
+
+   resetMoveDownTimer() {
+    if (this.moveDownTimer <= 0) {
+        this.moveDownTimer = this.moveDownTimerDefault;
+    }
+   }
+
+   decrementMoveDownTimer() {
+    if (
+        this.currentDirection === MovingDirection.downLeft ||
+        this.currentDirection === MovingDirection.downRight
+    ) {
+        this.moveDownTimer--;
+    }
    }
 
    updateVelocityAndDirection() {
@@ -39,10 +59,29 @@ export default class EnemyController {
             break;
         }
     } else if (this.currentDirection === MovingDirection.downLeft) {
-        this.xVelocity = 0;
-        this.yVelocity = this.defaultYVelocity;
-     }  
+        if (this.moveDown(MovingDirection.left)) {
+            break;
+        }
+     }  else if (this.currentDirection === MovingDirection.left) {
+        this.xVelocity = -this.defaultXVelocity;
+        this.yVelocity = 0;
+        const leftMostEnemy = enemyRow[0];
+        if (leftMostEnemy.x <= 0) {
+            this.currentDirection = MovingDirection.downRight;
+            break;
+      }
+     }
     }
+   }
+
+   moveDown(newDirection) {
+    this.xVelocity = 0;
+    this.yVelocity = this.defaultYVelocity;
+    if(this.moveDownTimer <= 0) {
+        this.currentDirection = newDirection;
+        return true;
+    }
+    return false;
    }
 
    drawEnemies(ctx) {
