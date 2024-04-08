40b63fe864f4d598fbce039577ebe837e4b8dbe0
Coby Walsh <cobywalsh7@yahoo.com>
2024-04-07 18:43:04 -0600

    created new Player.js file

---
+++ Player.js
@@ -0,0 +1,9 @@
+export default class Player {
+    constructor(canvas, velocity) {
+        this.canvas = canvas;
+        this.velocity = velocity;
+
+        this.x = this.canvas.width/2;
+        this.y = this.canvas.height -75;
+    }
+}
