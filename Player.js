abae4840da7eafe2900b0fdb59893347afad84f1
Coby Walsh <cobywalsh7@yahoo.com>
2024-04-07 18:50:54 -0600

    added player

--- Player.js
+++ Player.js
@@ -5,5 +5,13 @@ export default class Player {
 
         this.x = this.canvas.width/2;
         this.y = this.canvas.height -75;
+        this.width = 50;
+        this.height = 48;
+        this.image = new Image();
+        this.image.src = "images/player.png";
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
     }
 }
