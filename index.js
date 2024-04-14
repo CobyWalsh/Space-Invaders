96f931f30e210c7d2f1112ecae748b307cb55bb2
Coby Walsh <cobywalsh7@yahoo.com>
2024-04-14 09:06:05 -0600

    game over is now desplayed on the screen when player collides with enemy bullet

--- index.js
+++ index.js
@@ -16,12 +16,41 @@ const enemyBulletController = new bulletController(canvas, 4, "white", false);
 const enemyController = new EnemyController(canvas, enemyBulletController, playerBulletController);
 const player = new Player(canvas, 3, playerBulletController);
 
+let isGameOver = false;
+let didWin = false;
+
 function game() {
+checkGameOver();
 ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+displayGameOver();
+if (!isGameOver) {
 enemyController.draw(ctx);
 player.draw(ctx);
 playerBulletController.draw(ctx);
 enemyBulletController.draw(ctx);
+console.log(isGameOver);
+  }
+}
+
+function displayGameOver() {
+  if (isGameOver) {
+    let text = didWin ? "You Win" : "Game Over";
+    let textOffSet = didWin ? 3.5 : 5;
+
+    ctx.fillStyle = "red";
+    ctx.font = "70px Arial";
+    ctx.fillText(text, canvas.width / textOffSet, canvas.height / 2);
+  }  
+}
+
+function checkGameOver() {
+    if(isGameOver) {
+        return;
+    }
+
+    if (enemyBulletController.collideWith(player)) {
+        isGameOver = true;
+    }
 }
 
 setInterval(game, 1000 / 60);
